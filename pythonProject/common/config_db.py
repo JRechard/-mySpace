6ac04d5dd5a707c3ec6d4a88b338503043cf480d
gaozhiwei <15698392711@163.com>
2021-03-18 19:46:57 +0800

    config mongodb connection

--- pythonProject/common/config_db.py
+++ pythonProject/common/config_db.py
@@ -2,16 +2,24 @@ import pymongo
 
 
 class ConfigMongodb:
-    def __init__(self, host, port):
+    def __init__(self, host, port, username, password, db_name):
         self.host = host
         self.port = port
-        # self.admin = admin
-        # self.password = password
+        self.username = username
+        self.password = password
+        self.db_name = db_name
 
     def connect_db(self):
-        connection = pymongo.MongoClient(host=self.host, port=self.port)
-        return connection
+        conn = pymongo.MongoClient(
+            host=self.host,
+            port=self.port
+        )
+        database = conn[self.db_name]
+        database.authenticate(name=self.username, password=self.password)
+        return database
 
 
-client = ConfigMongodb("localhost", 27017)
-connection = client.connect_db()
+client = ConfigMongodb("123.206.217.183", 27017, "milu", "KuJ9i_of3hU_dGz3d", "mintmuse")
+db = client.connect_db()
+for i in db.list_collection_names():
+    print(i)
